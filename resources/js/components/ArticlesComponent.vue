8042fa31423f34ac4e784c3d15bca17d68a5a5a0
Aleksandar Savanovic <djaca85@gmail.com>
2019-04-18 15:24:07 +0200

    Add loader component

--- resources/js/components/ArticlesComponent.vue
+++ resources/js/components/ArticlesComponent.vue
@@ -1,36 +1,46 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <div v-for="(article, index) in articles" :key="index">
-        <div class="mb-3">
-          <h1 v-text="article.title"></h1>
-          <h6 v-if="article.author">by <a href="#" @click.prevent="filterBy(article.author)">{{ article.author.name }}</a></h6>
-        </div>
-        <div v-html="article.body"></div>
-        <div>
-          <span class="badge-pill badge-secondary">December 4, 2019</span>
+  <div>
+    <div class="card" v-if="articles.length > 0">
+      <div class="card-body">
+        <div v-for="(article, index) in articles" :key="index">
+          <div class="mb-3">
+            <h1 v-text="article.title"></h1>
+            <h6 v-if="article.author">by <a href="#" @click.prevent="filterBy(article.author)">{{ article.author.name }}</a></h6>
+          </div>
+          <div v-html="article.body"></div>
+          <div>
+            <span class="badge-pill badge-secondary">December 4, 2019</span>
+          </div>
+
+          <hr v-if="index !== articles.length - 1">
         </div>
 
-        <hr v-if="index !== articles.length - 1">
-      </div>
-
-      <div v-if="pagination" class="mt-5">
-        <button
-          v-show="pagination.next_page_url"
-          type="button"
-          class="btn btn-lg btn-block btn-outline-secondary"
-          @click="getArticles(pagination.next_page_url)"
-        >
-          Load more
-        </button>
+        <div v-if="pagination" v-show="!loading" class="mt-5">
+          <button
+            v-show="pagination.next_page_url"
+            type="button"
+            class="btn btn-lg btn-block btn-outline-secondary"
+            @click="getArticles(pagination.next_page_url)"
+          >
+            Load more
+          </button>
+        </div>
       </div>
     </div>
+
+    <v-loader :loading="loading" class="d-flex justify-content-center mt-5"></v-loader>
   </div>
 </template>
 
 <script>
+  import VLoader from './VLoader'
+
   export default {
     name: 'ArticlesComponent',
+
+    components: {
+      VLoader
+    },
 
     mounted () {
       this.getArticles()
---
+++ resources/js/components/VLoader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-show="loading">
+    <fade-loader :loading="loading" color="grey" size="30px"></fade-loader>
+  </div>
+</template>
+
+<script>
+  import FadeLoader from 'vue-spinner/src/FadeLoader.vue'
+
+  export default {
+    name: 'VLoader',
+
+    components: {
+      FadeLoader
+    },
+
+    props: ['loading']
+  }
+</script>
+
+<style scoped>
+
+</style>
