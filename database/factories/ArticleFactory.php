db2edb0ba948ec1e2f4e561d8795e4045a6ef65c
Aleksandar Savanovic <djaca85@gmail.com>
2019-04-23 12:50:31 +0200

    Fix handling fake images when seeding articles

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,12 +2,16 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 $factory->define(App\Article::class, function (Faker $faker) {
     return [
         'title'     => $faker->words(2, true),
         'body'      => $faker->realText(2000),
-        'excerpt'   => $faker->sentence,
+        'excerpt'   => $faker->text(100),
         'author_id' => function () {
             return factory(User::class)->create()->id;
         },
@@ -15,9 +19,25 @@ $factory->define(App\Article::class, function (Faker $faker) {
             if (app()->environment() === 'testing') {
                 return 'image.jpg';
             }
-            $img = $faker->image(storage_path('app/public/images'), 918, 400, null, false);
 
-            return $img;
+            $filepath = storage_path('app/public/images');
+
+            if(!File::exists($filepath)){
+                File::makeDirectory($filepath);
+            }
+
+            /*
+             * Default $faker->image() use https://lorempixel.com,
+             * sometimes is down or not getting image...
+             *
+             * https://github.com/fzaninotto/Faker/issues/957
+             */
+
+            $file = Image::make('https://loremflickr.com/918/400')->encode('jpg');
+
+            Storage::put($name = Str::random(40) . '.jpg', $file);
+
+            return $name;
         }
     ];
 });
